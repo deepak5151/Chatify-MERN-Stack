73e7df6d4fd468954d8110fd1b56853447e37c2c
Deepak Kapoor <nk93555@gmail.com>
2022-12-03 21:45:58 +0530

    Deploying to netlify

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -14,7 +14,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./helperComponents/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "https://https://my-chatify-app.herokuapp.com";
+const ENDPOINT = "https://my-chatify-app.netlify.app";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
