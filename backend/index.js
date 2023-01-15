7e623375215c92afea6204124a668bceaee2ba79
Deepak Kapoor <nk93555@gmail.com>
2023-01-15 22:08:12 +0530

    Fixed CORS error

--- backend/index.js
+++ backend/index.js
@@ -53,7 +53,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chatify-deepak-kapoor.vercel.app",
         // credentials: true,
     },
 });
