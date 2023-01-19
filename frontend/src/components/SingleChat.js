18bf6092aac17521f92663c3e71df1bdb1662bee
Deepak Kapoor <nk93555@gmail.com>
2023-01-19 17:22:18 +0530

    try3

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -104,7 +104,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
     };
 
     useEffect(() => {
-        socket = io(ENDPOINT);
+        socket = io();
         socket.emit("setup", user);
         socket.on("connected", () => setSocketConnected(true));
         socket.on("typing", () => setIsTyping(true));
