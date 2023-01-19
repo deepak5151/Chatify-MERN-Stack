d1467d2038daf6771807bcdb9903e1cc0f3a877e
Deepak Kapoor <nk93555@gmail.com>
2023-01-19 15:14:13 +0530

    Revert "fixed CORS"
    This reverts commit 50aa060b6b70ea4bc77df9b520b21afc9a28323d.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
-const cors = require('cors')
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
